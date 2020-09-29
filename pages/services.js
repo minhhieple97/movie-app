621bca164ed7de31a6b0ce05b1f3f2d5ce441888
HiepCongChua <hieplevuc@gmail.com>
2020-09-29 23:57:12 +0700

    update movie

--- pages/services.js
+++ pages/services.js
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 
 function services() {
-  return <div>Services</div>
+  return <h1>Services</h1>;
 }
 
-export default services
+export default services;
